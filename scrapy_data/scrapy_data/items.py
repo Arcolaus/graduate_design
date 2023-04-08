429c13799757994ba8ed1f26423c52f64a37c608
Arcolaus <46956750+Arcolaus@users.noreply.github.com>
2023-04-08 18:38:18 +0800

    edit data item class

--- scrapy_data/scrapy_data/items.py
+++ scrapy_data/scrapy_data/items.py
@@ -6,10 +6,23 @@
 import scrapy
 
 
-class ScrapyDataItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    movie_name=scrapy.Field()
-    movie_url=scrapy.Field()
-    
-    pass
+class Movie(scrapy.Item):
+	# define the fields for your item here like:
+	# name = scrapy.Field()
+	movie_name=scrapy.Field()
+	movie_url=scrapy.Field()
+
+
+class MovieDetail(scrapy.Item):
+	movie_title = scrapy.Field()        # 片名
+	movie_detail_url=scrapy.Field()     # 详情页url
+	movie_id=scrapy.Field()             # 豆瓣电影唯一标识id
+	movie_final_score=scrapy.Field()    # 得分
+	movie_score_num=scrapy.Field()    	# 评分人数
+	movie_comment_num=scrapy.Field()	# 短评数
+
+class MovieComment(scrapy.Item):		
+	comment_score=scrapy.Field()		# 给出的打分
+	comment_stars=scrapy.Field()		# 获得的“有用”
+	comment_movie_id=scrapy.Field()		# 对应的电影id
+	comment_content=scrapy.Field()		# 短评内容（需要清洗，去除各种转义符
